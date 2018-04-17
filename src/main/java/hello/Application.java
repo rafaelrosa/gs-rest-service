d3f5ee66abf5d2625d7058b5a4f81922f12e2ced
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2018-04-17 02:13:36 -0300

    Inclusion of ComponentScan and EnableAsync annotations

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,8 +2,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@ComponentScan(basePackages={"hello"})
 public class Application {
 
     public static void main(String[] args) {
