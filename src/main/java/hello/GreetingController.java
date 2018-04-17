9279320a67ca27e2fe4cb52ada1fb375985287f2
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2018-04-17 02:14:26 -0300

    Inclusion of post endpoint in GreetingControler

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,7 +1,9 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,16 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping(value="/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    
+    @PostMapping(value="/test-post")
+    public String testPost() {
+    	int x = (int) (System.currentTimeMillis() % 57);
+    	Greeting g = new Greeting(x, "Greeting "+x);
+    	return g.toString();
     }
 }
